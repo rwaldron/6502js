a9502d46d32d8c26ded0a09e4cc94ac7966ba7a2
Rick Waldron <waldron.rick@gmail.com>
2016-10-25 09:07:50 -0400

    Use built-in toString(16) to make hex value

--- assembler.js
+++ assembler.js
@@ -2471,10 +2471,7 @@ function SimulatorWidget(node) {
   }
 
   function num2hex(nr) {
-    var str = "0123456789abcdef";
-    var hi = ((nr & 0xf0) >> 4);
-    var lo = (nr & 15);
-    return str.substring(hi, hi + 1) + str.substring(lo, lo + 1);
+    return nr.toString(16);
   }
 
   // message() - Prints text in the message window
